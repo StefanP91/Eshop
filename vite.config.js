5b96f15169c4458bf85e4eeb029f089fbb5db5a2
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2025-02-02 14:55:58 +0100

    vite confing add build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Ehop/',
+  build: {
+    outDir: "dist",
+  },
   test:{
     browser: {
       name: 'brave',
