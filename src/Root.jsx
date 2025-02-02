0f2d1b54dd160b47253d55822fab505c8a63deca
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2025-02-02 15:33:37 +0100

    delete testing h1

--- src/Root.jsx
+++ src/Root.jsx
@@ -5,7 +5,6 @@ import Footer from "./components/Footer"
 const Root = () => {
     return (
         <>
-        <h1>Welcome</h1>
             <Header />
             <Main />
             <Footer />
