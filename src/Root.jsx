ec473e98b0b7c750e3e6599458d2d0278372478f
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2025-02-02 13:48:48 +0100

    h1 element

--- src/Root.jsx
+++ src/Root.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer"
 const Root = () => {
     return (
         <>
+        <h1>Welcome</h1>
             <Header />
             <Main />
             <Footer />
