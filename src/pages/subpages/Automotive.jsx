e05d4973b68f801979a71102ffac81d740bc5ca9
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2025-02-04 13:24:24 +0100

    update responce in automotive page

--- src/pages/subpages/Automotive.jsx
+++ src/pages/subpages/Automotive.jsx
@@ -17,7 +17,8 @@ const Automotive = () => {
 
         const fetchProducts = async () => {
             try {
-                const response = await fetch(`https://webshop.free.nf/automotive_page.php`); 
+                const response = await fetch(`https://webshop.free.nf/automotive_page.php`, {mode: 'cors'}); 
+                
                 if (!response.ok) {
                     throw new Error('Failed to fetch product');
                 }
