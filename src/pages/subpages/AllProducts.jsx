71e2400904c666e2cf52cc4e6096c4b82dcd4b99
Stefan Panov <64032523+StefanP91@users.noreply.github.com>
2025-02-05 21:27:23 +0100

    api change

--- src/pages/subpages/AllProducts.jsx
+++ src/pages/subpages/AllProducts.jsx
@@ -17,7 +17,7 @@ const AllProducts = () => {
 
         const fetchProducts = async () => {
           try {
-            const response = await fetch(`https://api-eshop.herokuapp.com/all_products_page.php`);
+            const response = await fetch(`https://webshop.free.nf/all_products_page.php`);
 
             if (!response.ok) {
               throw new Error('Failed to fetch product');
